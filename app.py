1677060b83dcd5ab7e54f7ef195a0adf53c3bf89
vin019 <bacsarsav@gmail.com>
2024-11-23 20:58:16 +0800

    4th commit from data loss

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
 from config import Config
